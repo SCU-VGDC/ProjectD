2d574366f546953342fec09e611aabdd799cb8bb
Kara Barrese <karabarrese@gmail.com>
2023-02-17 17:20:31 -0800

    Working orb behavior with times

--- Assets/Scripts/Dash_Orb_Behavior.cs
+++ Assets/Scripts/Dash_Orb_Behavior.cs
@@ -8,6 +8,9 @@ namespace Player_Movement_Namespace
     {
         public GameObject player;
         public Player_Movement player_movement;
+        public float time_orb_gone = 0f; 
+        public float orb_cool_down = 5f; // 5 seconds
+        public bool see = false;
 
         // Start is called before the first frame update
         void Start()
@@ -16,16 +19,37 @@ namespace Player_Movement_Namespace
             player_movement = player.GetComponent<Player_Movement>();
         }
 
-        //make orb dissapear when you touch it?
+        void orb_visibility()
+        {
+            GetComponent<Collider2D>().enabled = see;
+            GetComponent<SpriteRenderer>().enabled = see;
+        }
+
+        void Update()
+        {
+            //controls timer for respawn
+            if(!see)
+            {
+                if(time_orb_gone >= orb_cool_down)
+                {
+                    see = true;
+                    orb_visibility();
+                }
+                time_orb_gone += Time.deltaTime;
+            }
+        }
+
+        //orb dissapears after touch for 5s
         void OnTriggerEnter2D(Collider2D hit_info)
         {
-            //if the enemy bullet hits a platform, the bounds, destroy itself
+            //tells if player (on layer 7) is in orb
             if(hit_info.gameObject.layer == 7)
             {
                 Debug.Log("touched orb!");
                 Debug.Log(player_movement.current_dashes);
                 Debug.Log(player_movement.maximum_dashes);
 
+                
                 if(player_movement.current_dashes<player_movement.maximum_dashes)
                 {
                     player_movement.current_dashes+=1;
@@ -35,7 +59,9 @@ namespace Player_Movement_Namespace
                     Debug.Log("already at max dashes!");
                 }
 
-                Destroy(gameObject);
+                see = false;
+                orb_visibility();
+                time_orb_gone = 0f;
             }
         }
     }
--- Assets/Scripts/Dash_Through_Orb_Behavior.cs
+++ Assets/Scripts/Dash_Through_Orb_Behavior.cs
@@ -8,6 +8,9 @@ namespace Player_Movement_Namespace
     {
         public GameObject player;
         public Player_Movement player_movement;
+        public float time_orb_gone = 0f; 
+        public float orb_cool_down = 5f; // 5 seconds
+        public bool see = false;
 
         // Start is called before the first frame update
         void Start()
@@ -16,27 +19,42 @@ namespace Player_Movement_Namespace
             player_movement = player.GetComponent<Player_Movement>();
         }
 
-        //make orb dissapear when you touch it?
+        //changes orb status
+        void orb_visibility()
+        {
+            GetComponent<Collider2D>().enabled = see;
+            GetComponent<SpriteRenderer>().enabled = see;
+        }
+
+        void Update(){
+            //controls timer for respawn
+            if(!see)
+            {
+                if(time_orb_gone >= orb_cool_down)
+                {
+                    see = true;
+                    orb_visibility();
+                }
+                time_orb_gone += Time.deltaTime;
+            }
+        }
+
         void OnTriggerEnter2D(Collider2D hit_info)
         {
-            //if the enemy bullet hits a platform, the bounds, destroy itself
+            //if the player is dashing and goes through, it'll dissapear for 5s
             if(hit_info.gameObject.layer == 7)
             {
-                if(player_movement.is_dashing){
-                    Debug.Log("touched orb dashing orb!");
-                    Debug.Log(player_movement.current_dashes);
-                    Debug.Log(player_movement.maximum_dashes);
-
-                    if(player_movement.current_dashes<player_movement.maximum_dashes)
+                if(player_movement.is_dashing)
+                {
+                    // add one if you aren't at max
+                    if(player_movement.current_dashes<player_movement.maximum_dashes) 
                     {
                         player_movement.current_dashes++;
                     }
-                    else
-                    {
-                        Debug.Log("already at max dashes!");
-                    }
 
-                    Destroy(gameObject);
+                    see = false;
+                    orb_visibility();
+                    time_orb_gone = 0f;
                 }
                 
             }
