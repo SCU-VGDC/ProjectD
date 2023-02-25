6c8a6b341eebcc5275ed24e690ffb3cfe8c0069f
Rand0mNigel <nigelsmeyer@icloud.com>
2023-02-24 16:53:05 -0800

    Added Checkpoints and a simple death system
    Added checkpoint objects that can be teleported to by the Player.

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player_Movement_Namespace {
+
+    public class Checkpoint : MonoBehaviour
+    {
+        [SerializeField] private GameObject player;
+        
+        [SerializeField] private Collider2D playerCollider;
+        [SerializeField] private GameObject thisCheckPoint;
+        [SerializeField] private Collider2D checkpointCollider;
+        [SerializeField] public Player_Movement playerMovement;
+        void Start(){
+            playerCollider = GetComponent<Collider2D>();
+            checkpointCollider = GetComponent<Collider2D>();
+            playerMovement = player.GetComponent<Player_Movement>();
+            //thisCheckPoint = GetComponent<Checkpoint>();
+
+
+        }
+
+        void OnTriggerEnter2D(Collider2D Other) {
+            //players hitbox is hit updates checkpoint
+            
+            if (Other.gameObject.layer == 7){
+                playerMovement.setCheckpoint(thisCheckPoint);
+                Debug.Log("Checkpoint set to " + playerMovement.getCheckPoint());
+            }
+        }
+
+
+
+
+
+
+   }
+}
