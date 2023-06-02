948f2cb31ce59aa7f30db5366763495b19aa3396
Andrew Collins <56804602+aceeedev@users.noreply.github.com>
2023-06-02 16:17:33 -0700

    Fixed enemy list in room manager (#50)
    Also made dash UI part of main canvas, not player canvas and cleaned up pd code

--- Assets/Scripts/Enemy/Enemy_Health.cs
+++ Assets/Scripts/Enemy/Enemy_Health.cs
@@ -50,18 +50,24 @@ public class Enemy_Health : MonoBehaviour
 
     public void Die()
     {
-        if(transform.parent != null)
+        if (transform.parent != null)
         {
             if (transform.parent.parent != null)
             {
-                if(transform.parent.parent.GetComponent<RoomManager>())
+                if (transform.parent.parent.GetComponent<RoomManager>())
                 {
                     transform.parent.parent.GetComponent<RoomManager>().enemies.Remove(this);
-                    transform.parent.parent.GetComponent<RoomManager>().amountOfEnemies--;
                 }
             }
             Destroy(transform.parent.gameObject);
+        } else 
+        {
+            if (transform.parent.GetComponent<RoomManager>())
+            {
+                transform.parent.GetComponent<RoomManager>().enemies.Remove(this);
+            }
         }
+
         Destroy(gameObject);
     }
 }
