e9caec75e486fe4b051b1c7854bc8427d40c6c13
Andrew Collins <56804602+aceeedev@users.noreply.github.com>
2023-10-10 10:39:45 -0700

    Updated Room Manager
    Created a wave system and rewrote code to be in a single file

--- Assets/Scripts/Manager/RoomManager.cs
+++ Assets/Scripts/Manager/RoomManager.cs
@@ -3,97 +3,181 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Wave {
+    // an empty game object which contains all the enemies as children 
+    public GameObject waveGameObject;
+
+    // the delay before the wave starts
+    public float waveDelayTime = 0.0f;
+    // the delay between enemies
+    public float enemyDelayTime = 0.0f;
+
+    public bool waveOver 
+    { 
+        get 
+        { 
+            if (waveGameObject == null) return false;
+
+            return waveGameObject.transform.childCount == 0; 
+        } 
+        set { value = waveOver; } 
+    }
+
+    public Wave(GameObject initialWaveGameObject, float initialWaveDelayTime, float initialEnemyDelayTime) 
+    {
+        waveGameObject = initialWaveGameObject;
+        waveDelayTime = initialWaveDelayTime;
+        enemyDelayTime = initialEnemyDelayTime;
+
+        waveOver = false;
+
+        // make all enemies not enabled
+        foreach(Transform enemy in waveGameObject.transform) 
+        {
+            enemy.gameObject.SetActive(false);
+        }
+    }
+}
+
 public class RoomManager : MonoBehaviour
 {
-    public int id; //room id to save
-    public bool battleStarted;  //if battle started
-    public List<ActorHealth> enemies; //list of enemies elements
+    private bool inBattle;  // if battle started
+    private bool m_inBattle;
+    public List<Wave> waves = new List<Wave>();
+    private int waveNum;
+    private IEnumerator coroutineWave;
+    private IEnumerator coroutineSpawnWaves;
 
+    private GameObject player;
+    public GameObject doorCloser;
     private GameObject entrance;
     private GameObject exit;
-    private bool m_battleStarted;
-   
+
+    public float doorCloserDistance = 0.3f;
+
+    public float waveDelayTime = 2.5f;
+    public float enemySpawnDelayTime = 1.0f;
 
     void Start()
     {
-        foreach (Transform enem in transform) //finds enemies and entrance and exit
-        {
-            // if enemy's children does contain enemy health script
-            if (enem.transform.childCount != 0)
-            {
-                // check parent for enemy health script
-                if (enem.GetComponent<ActorHealth>())
-                {
-                    addToEnemyList(enem);
-                }
+        player = GameManager.inst.player;
 
-                // check children for enemy health script
-                foreach (Transform undprefab in enem.transform)
-                {
-                    if (undprefab.GetComponent<ActorHealth>())
+        foreach (Transform child in transform) // iterate over children
+        {
+            switch(child.gameObject.name) 
+            {
+                case "DoorCloser":
+                    doorCloser = child.gameObject;
+                    break;
+
+                case "Entrance":
+                    entrance = child.gameObject;
+                    break;
+
+                case "Exit":
+                    exit = child.gameObject;
+                    break;
+
+                default:
+                    if (child.name.Contains("Wave"))
                     {
-                        addToEnemyList(undprefab);
+                        waves.Add(new Wave(child.gameObject, waveDelayTime, enemySpawnDelayTime));
                     }
-                }
-            }
 
-            if (enem.name == "Entrance")
-            {
-                entrance = enem.gameObject;
-            }
-            if (enem.name == "Exit")
-            {
-                exit = enem.gameObject;
+                    break;
             }
         }
 
+        // default the gates open
         entrance.SetActive(false);
-        exit.SetActive(false);
+        exit.SetActive(true);
     }
 
-    void addToEnemyList(Transform enem)
-    {
-        enemies.Add(enem.GetComponent<ActorHealth>());
-        enem.gameObject.SetActive(false);
-    }
-
-    public void Startbattle() //startbattle trigger
-    {
-        if(enemies.Count > 0)
-        {
-            battleStarted = true;
-        }
-    }
-
-    
     void FixedUpdate()
     {
-        if(enemies.Count == 0)
+        // check if player has entered the arena
+        float playerDist = Vector2.Distance(player.transform.position, doorCloser.transform.position);
+        if(playerDist < doorCloserDistance)
         {
-            battleStarted = false; //reset if no enemies
+            inBattle = true;
         }
 
-        if (battleStarted != m_battleStarted) //check if battle started have been changed
+        //check if battle started has been changed from last state
+        if (inBattle != m_inBattle)
         {
 
-            if (battleStarted == false)
+            if (!inBattle) // battle has ended
             {
                 entrance.SetActive(false); //open doors
                 exit.SetActive(false);
             }
 
 
-            if (battleStarted == true) //set active all enemies and close doors
+            if (inBattle) //set active all enemies and close doors
             {
-                foreach(ActorHealth i in enemies)
-                {
-                    i.gameObject.SetActive(true);
-                }
+                // boxed like a fish
                 entrance.SetActive(true);
                 exit.SetActive(true);
+
+                coroutineSpawnWaves = spawnWaves();
+                StartCoroutine(coroutineSpawnWaves);
             }
 
-            m_battleStarted = battleStarted; //save the last value
+            m_inBattle = inBattle; //save the last value
         }
+    }
+
+    /// <summary>
+    /// Spawns an individual wave
+    /// </summary>
+    /// <param name="wave"></param>
+    /// <returns></returns>
+    private IEnumerator spawnWave(Wave wave)
+    {
+        yield return new WaitForSeconds(wave.waveDelayTime);
+
+        // spawn enemies
+        foreach(Transform enemy in wave.waveGameObject.transform) 
+        {
+            enemy.gameObject.SetActive(true);
+
+            yield return new WaitForSeconds(wave.enemyDelayTime);
+        }
+    }
+
+    /// <summary>
+    /// Spawns each wave sequentially
+    /// </summary>
+    /// <param name="wave"></param>
+    /// <returns></returns>
+    private IEnumerator spawnWaves()
+    {
+        for (waveNum = 0; waveNum < waves.Count; waveNum++) 
+        {
+            Wave currentWave = waves[waveNum];
+
+            coroutineWave = spawnWave(currentWave);
+            StartCoroutine(coroutineWave);
+
+            // wait until wave has been completed (all enemies are dead)
+            while (!currentWave.waveOver)
+            {
+                yield return null;
+            }
+        }
+
+        // be free
+        exit.SetActive(false);
+    }
+
+
+    void OnDrawGizmos()
+    {
+            if (doorCloser != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(doorCloser.transform.position, doorCloserDistance);
+            }
+   
     }
 }
