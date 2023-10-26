f9d78766f9f044c06e6c69a9487541b40c105899
savasva <savasva@mail.ru>
2023-10-25 20:31:58 -0700

    Added a very simple paralax effect, needs finetuning for level one and so on

---
+++ Assets/Scripts/Parallax/ParralexEffect.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ParralexEffect : MonoBehaviour
+{
+
+    private List<Transform> parLayers;
+    private Vector3 origCamPos;
+
+    void SetUpLayer()
+    {
+        parLayers = new List<Transform>();
+
+        foreach (Transform child in transform)
+        {
+            parLayers.Add(child);
+        }
+
+        origCamPos = Camera.main.transform.position;
+
+        foreach (Transform v in parLayers)
+        {
+            v.position = origCamPos;
+        }
+    }
+
+    void Start()
+    {
+        SetUpLayer();
+    }
+
+    void MoveLayer(int LayerId)
+    {
+        Vector3 DistanceChange = Camera.main.transform.position - origCamPos;
+
+        float moveFactor = (((float)(LayerId + 1)) / (parLayers.Count + 1));
+
+        Vector3 AppliedPosition = DistanceChange * moveFactor;
+
+        if (LayerId == 0)
+        {
+            AppliedPosition = DistanceChange;
+        }
+
+        parLayers[LayerId].position = AppliedPosition;
+    }
+    void LateUpdate()
+    {
+        for(int i = 0; i < parLayers.Count; i++)
+        {
+            MoveLayer(i);
+        }
+    }
+}
