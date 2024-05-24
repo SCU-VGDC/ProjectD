f4c9bfcf573074a78fa0dd64659d87d9fcf8aec2
MaxWhite2024 <mjwhite@scu.edu>
2024-05-23 22:02:36 -0700

    Key pickup working

--- Assets/Scripts/Player/PlayerLockInventory.cs
+++ Assets/Scripts/Player/PlayerLockInventory.cs
@@ -8,24 +8,26 @@ public class PlayerLockInventory : MonoBehaviour
     public Dictionary<string, bool> playerKeys;
 
     // Start is called before the first frame update
-    // void Start()
-    // {
-    //     //make playerKeys an array of size 3
-    //     playerKeys = new Dictionary<string, bool>();
-    // }
+    void Start()
+    {
+        //create a dictionary
+        playerKeys = new Dictionary<string, bool>();
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         //if other collider was a key...
         if(col.gameObject.GetComponent<KeyBehavior>())
         {
-            Debug.Log("Key Touched");
-            Debug.Log(col.gameObject.GetComponent<KeyBehavior>().keyType);
+            // Debug.Log("Key Touched");
+            // Debug.Log(col.gameObject.GetComponent<KeyBehavior>().keyType);
+
             //add key to inventory
             playerKeys.Add(col.gameObject.GetComponent<KeyBehavior>().keyType, true);
-            Debug.Log(playerKeys.ContainsKey("bronze"));
-            Debug.Log(playerKeys.ContainsKey("silver"));
-            Debug.Log(playerKeys.ContainsKey("gold"));
+            
+            // Debug.Log(playerKeys.ContainsKey("bronze"));
+            // Debug.Log(playerKeys.ContainsKey("silver"));
+            // Debug.Log(playerKeys.ContainsKey("gold"));
 
             //destroy key
             Destroy(col.gameObject);
