5f65fa6eef4ffbb4d9fef5f64cfe88490a8c8107
MaxWhite2024 <mjwhite@scu.edu>
2024-05-31 19:05:19 -0700

    save

--- Assets/Scripts/Environment/KeyBehavior.cs
+++ Assets/Scripts/Environment/KeyBehavior.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class KeyBehavior : MonoBehaviour
 {
+    //type of the key
     public string keyType = "";
+
+    private Transform keySpriteTrans;
+    private Transform keyLightTrans;
+
+    void Start()
+    {
+        //get key light obj
+        keyLightTrans = transform.GetChild(0);
+
+        //get key sprite obj
+        keySpriteTrans = transform.GetChild(1);
+    }
+
+    void Update()
+    {
+        //undulate light up and down
+        keyLightTrans.position += new Vector3(0f, Mathf.Sin(Time.deltaTime), 0f);
+
+        //undulate sprite up and down
+        keySpriteTrans.position += new Vector3(0f, Mathf.Sin(Time.deltaTime), 0f);
+    }
 }
