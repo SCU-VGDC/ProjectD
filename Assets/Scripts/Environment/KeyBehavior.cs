994f09d940f130a7003bfcbc9e8cb757cc6e0f90
MaxWhite2024 <mjwhite@scu.edu>
2024-05-31 22:31:04 -0700

    better key animation

--- Assets/Scripts/Environment/KeyBehavior.cs
+++ Assets/Scripts/Environment/KeyBehavior.cs
@@ -7,24 +7,35 @@ public class KeyBehavior : MonoBehaviour
     //type of the key
     public string keyType = "";
 
-    private Transform keySpriteTrans;
+    public float bobMagnitude = 1f;
+    public float bobSpeed = 1f;
+
     private Transform keyLightTrans;
+    private float originalLightY;
+    private Transform keySpriteTrans;
+    private float originalSpriteY;
 
     void Start()
     {
         //get key light obj
         keyLightTrans = transform.GetChild(0);
 
+        //get key light y
+        originalLightY = keyLightTrans.position.y;
+
         //get key sprite obj
         keySpriteTrans = transform.GetChild(1);
+
+        //get key sprite y
+        originalSpriteY = keySpriteTrans.position.y;
     }
 
     void Update()
     {
         //undulate light up and down
-        keyLightTrans.position += new Vector3(0f, Mathf.Sin(Time.deltaTime), 0f);
+        keyLightTrans.position = new Vector3(keyLightTrans.position.x, bobMagnitude * Mathf.Sin(Time.time * bobSpeed) + originalLightY, 0f);
 
         //undulate sprite up and down
-        keySpriteTrans.position += new Vector3(0f, Mathf.Sin(Time.deltaTime), 0f);
+        keySpriteTrans.position = new Vector3(keySpriteTrans.position.x, bobMagnitude * Mathf.Sin(Time.time * bobSpeed) + originalSpriteY, 0f);
     }
 }
