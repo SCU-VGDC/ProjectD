4c6d498f9300ca1d2e4d314c2a02d5054cefd78f
MaxWhite2024 <mjwhite@scu.edu>
2024-08-11 16:24:06 -0700

    Revert "Merge branch 'gear-changes' into dev"
    This reverts commit c36b9a647b29d845b182faad23884a30b948f1c1, reversing
    changes made to 98a6aec12e9765d0644536aa5873a017132b09e5.

--- Assets/Scripts/Environment/MoveGearPlatforms.cs
+++ Assets/Scripts/Environment/MoveGearPlatforms.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoveGearPlatforms : MonoBehaviour
@@ -12,9 +11,6 @@ public class MoveGearPlatforms : MonoBehaviour
 
     public float speed = 3f;
 
-    private float timer;
-    private float timerchange=1f;// this is the ammount of time to see how long before it moves back.
-
     private Vector3 target;
 
     void Start() 
@@ -23,34 +19,18 @@ public class MoveGearPlatforms : MonoBehaviour
 
         platform.position = start.position;
         target = start.position;
-        timer=0;//makes the timer start at zero initallty so it won't.
-        
     }
 
     void FixedUpdate() 
     {
         Vector3 currentPos = platform.position;
-        timer+=Time.deltaTime;
-        //checks if timer is greatera than change and moves towards the start.
-        if(timer>timerchange)
-        {
-            platform.position=Vector3.MoveTowards(platform.position,start.position,speed*Time.deltaTime);
-        }
-        else
-        {
-            platform.position=Vector3.MoveTowards(platform.position,end.position,speed*Time.deltaTime);
-        }
-        /*
+ 
         if(currentPos != target) 
         {
             platform.position = Vector3.MoveTowards(platform.position, target, speed * Time.deltaTime);
         }
-        */
     }
-    public void collided()//checks collider if it hits Check the event manager it should have the call for when it collides.
-    {
-        timer=0f;
-    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         other.transform.SetParent(platform, true);
